65879450c0937caf04a533e98cc4ff7ae9976a8b
Taery <Taery@inClass>
2025-06-02 20:29:41 +0900

    asdf

---
+++ TargetNumber.py
@@ -0,0 +1,14 @@
+class node():
+    def __init__(self,value):
+        self.r
+        self.l
+        self.value=value
+def solution(numbers, target):
+    answer = 0
+    sign=[]
+    for i in numbers:
+        sign.append(1)
+    for i in numbers:
+        sum+=numbers[i]*sign[i]
+
+    return answer
