92cc3721239abd778cfd2a9db9ca4b3e79476dbd
Taery <tae5431@gmail.com>
2025-06-04 20:39:46 +0900

    asdf

---
+++ 2025_06/Algorithm/ALDS1_2_D.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+    //insertionSort(A,n,g)
+}
+int insertionSort(A,n,g){
+    for(int i=g;i<n;i++){
+        v=A[i];
+        j=i-g
+        while(j>=0 && A[j]>v){
+            A[j+g]=A[j]
+
+        }
+    }
+}
