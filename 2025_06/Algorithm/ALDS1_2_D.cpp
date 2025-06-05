d5ffeee750a703dca10875af810378b86e8318b0
Taery <tae5431@gmail.com>
2025-06-05 10:08:42 +0900

    asdf

--- 2025_06/Algorithm/ALDS1_2_D.cpp
+++ 2025_06/Algorithm/ALDS1_2_D.cpp
@@ -3,16 +3,35 @@
 #include<stdio.h>
 using namespace std;
 
+int shellSort(vector<int> A,int n);
+
 int main(){
-    //insertionSort(A,n,g)
+    vector<int> A;
+    int n=0;
+    shellSort(A,n);
 }
-int insertionSort(A,n,g){
+int cnt=0;
+int insertionSort(vector<int> A,int n,int g)
+{
+    int j=0;
+    int v=0;
     for(int i=g;i<n;i++){
         v=A[i];
-        j=i-g
+        j=i-g;
         while(j>=0 && A[j]>v){
-            A[j+g]=A[j]
-
+            A[j+g]=A[j];
+            cnt++;
         }
+        A[j+g]=v;
     }
 }
+int shellSort(vector<int> A,int n){
+    cnt=0;
+    int m=-1;
+    vector<int> G={};
+    for(int i=0;i<m;i++){
+        insertionSort(A,n,G[i]);
+    }
+}
+
+
