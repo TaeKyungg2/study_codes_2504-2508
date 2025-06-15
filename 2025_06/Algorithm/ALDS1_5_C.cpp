c8094f3d87e5acbe0f32dc4031c0cf0d3c389cb0
Taery <tae5431@gmail.com>
2025-06-16 00:56:20 +0900

    asdf

--- 2025_06/Algorithm/ALDS1_5_C.cpp
+++ 2025_06/Algorithm/ALDS1_5_C.cpp
@@ -15,10 +15,10 @@ void koch(int n,vector<pair<float,float>> &point)
     float y = point[i].second;
     float xx= point[point.size()-i-1].first;
     float yy= point[point.size()-i-1].second;
-    
+
     point.insert(point.begin()+i,{(xx-x)/3,(yy-y)/3});
     point.insert(point.begin()+i+1,{(xx-x)/3*2,(yy-y)/3*2});
-    point.insert(point.begin()+i,{(xx-x)/2,(xx-x)/3*sin(PI/3)})
+    point.insert(point.begin()+i,{(xx-x)/2,(xx-x)/3*sin(PI/3)});
 }
 
 int main()
