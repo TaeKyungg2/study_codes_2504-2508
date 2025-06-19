a9a5d92402c248957c19439c4f03214a366de0c0
Taery <tae5431@gmail.com>
2025-06-19 23:59:55 +0900

    mylife

--- 2025_06_First_Half/Algorithm/sample.cpp
+++ 2025_06_First_Half/Algorithm/sample.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <iterator> // for std::size
-
+using namespace std;
+int n;
 int main() {
-    std::vector<int> v = {1,2,3};
-    std::cout << "size:  " << v.size() << "\n";
+    vector<int> v = {1,2,3};
+    cout << "size:  " << v.size() << "\n";
+    cin >> n;
+    cout << "n:  " << n << "\n";
+    int a,b;
+    cout << n << endl;
+    return 0;
 }
