007f2272b6eb6f16a9beba5eb78a23ae5e808027
Taery <tae5431@gmail.com>
2025-06-20 03:12:38 +0900

    asdf

--- 2025_06_Second_Half/Algorithm/ALDS1_10_C.py
+++ 2025_06_Second_Half/Algorithm/ALDS1_10_C.py
@@ -1,11 +1,16 @@
 def dynamic(fir,sec):
     dp=[len(fir)*[0]]*len(sec)
     a,b=0,0
+    match=False
     for j in sec:
         b=0
+        match=False
         for z in fir:
+            if match : 
+                dp[a][b]=dp[a][b-1]
+                continue
             x=1 if j==z else 0
-
+            if j==z : match=True
             if j==z and a!=0 and b!=0:dp[a][b]=dp[a-1][b-1]+1
             elif j==z and a!=0 : dp[a][b]=dp[a-1][b]+1
             elif j==z and b!=0: dp[a][b]=dp[a][b-1]+1
