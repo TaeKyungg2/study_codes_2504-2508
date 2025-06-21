d93e2e54a05c936974e84ec087718af27ee794be
Taery <tae5431@gmail.com>
2025-06-21 16:53:03 +0900

    ads

--- 2025_06_Second_Half/Algorithm/ALDS1_11_B.py
+++ 2025_06_Second_Half/Algorithm/ALDS1_11_B.py
@@ -13,6 +13,7 @@ time=[[0,0] for _ in range(len(arr))]
 ii=1
 def DFSarr(node):
     global ii,arr,time
+    if time[node-1][0]!=0 : return
     time[node-1][0]=ii
     ii+=1
     for i in arr[node-1][2:]:
@@ -22,8 +23,14 @@ def DFSarr(node):
     ii+=1
 
 DFSarr(arr[0][0])
-for i in range(len(time)):
+i=1
+flag=[0,0] in time
+while flag:
     if time[i][0]==0:
         DFSarr(arr[i][0])
+        if [0,0] not in time:break
+    i+=1
+    i%=len(time)
+
 for i in arr:
     print(f'{i[0]} {time[i[0]-1][0]} {time[i[0]-1][1]}')
