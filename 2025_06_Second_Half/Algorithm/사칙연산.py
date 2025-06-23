8fbf2bfaaf6802ebe9250a3bbca3ffcaad04e639
TaeryClassCom <tae5431@gmail.com>
2025-06-23 17:53:53 +0900

    asdf

---
+++ 2025_06_Second_Half/Algorithm/사칙연산.py
@@ -0,0 +1,37 @@
+# 1 2 3 4 ->5  length-1==len
+#     len-1(2블록)+len-2(3블록)+1(3블록의 자식)
+# 1 2 3 4 5 ->    len=4
+#     len-1(2블록)+len-2(3블록)+len-3(4블록)
+# 3+2+1
+# 3(1)    
+# 4(2+1+1)
+
+# f=시그마 1~len-1 + 시그마(시그마 1~len-1)
+
+# - 가 있어야 괄호 의미가 있다.
+dict={}
+class Pair():
+    def __init__(self,fir,two):
+        self.one=fir
+        self.two=two
+class Pairs():
+    def __init__(self):
+        self.p=[]
+def solution(arr):
+    answer = -1
+    def choose(o,t,pairs):
+        if t-o<2:
+            return
+        pairs.p.append(Pair(o,t))
+        if pairs in dict:
+            return
+        for i in range(o+1,t):
+            choose(i,t,pairs)
+
+            
+    pairs=Pairs()
+    choose(1,len(arr),pairs)
+
+    return answer
+def calcul(str,pairs):
+    cal.insert()
--- 2025_06_Second_Half/Algorithm/정수_삼각형.py
+++ 2025_06_Second_Half/Algorithm/정수_삼각형.py
@@ -1,19 +1,21 @@
-
+#https://school.programmers.co.kr/learn/courses/30/lessons/43105
 def solution(triangle):
-    def dyna(n,m,sum):
+    def dyna(n,m):
         if sum_tra[n][m]!=0:
             return sum_tra[n][m]
         else:
-            sum+=triangle[n][m]
-            sum_tra[n][m]=\
-                sum if n==len(triangle)-1 else \
-                max(dyna(n+1,m,sum),dyna(n+1,m+1,sum))
-            return sum_tra[n][m]
+            if n!=len(triangle)-1 :
+                v1=dyna(n+1,m)
+                v2=dyna(n+1,m+1)
+                sum_tra[n][m]=max(v1,v2)+triangle[n][m]
+                return sum_tra[n][m]
+            else:
+                return triangle[n][m]
     answer = 0
     sum_tra=[]
     for i in range(len(triangle)):
         sum_tra.append([0]*(i+1))
-    answer=dyna(0,0,0)
+    answer=dyna(0,0)
     return answer
 
 print(solution([[7], [3, 8], [8, 1, 0]\
