a577cb7a9e9d0ccaefd198069baadf388870283c
TaeryClassCom <tae5431@gmail.com>
2025-06-26 17:57:39 +0900

    asdf

---
+++ 2025_06_Second_Half/Algorithm/ALDS1_12_A.py
@@ -0,0 +1,33 @@
+import heapq
+
+def RowToInt():
+    nums=[]
+    nums=list(map(int,input().split()))
+    return nums
+
+def NRowToInt():
+    n=int(input())
+    nums_=[]
+    for i in range(n):
+        nums_.append(RowToInt())
+    return nums_
+def GoSmall(node):
+    global box,visit,sum
+    visit[node]=True
+    for i,pri in enumerate(A[node]):
+        if pri==-1:continue
+        heapq.heappush(box,(pri,i))
+    
+    while len(box)>0:
+        pri,next=heapq.heappop(box)
+        if visit[next]:continue
+        sum+=pri
+        GoSmall(next)
+
+A=NRowToInt()
+box=[]
+visit=[False]*len(A)
+sum=0
+GoSmall(0)
+print(sum)
+
