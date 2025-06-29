50c5bf10795e6505d07731b9be410830d6b324c5
Taery <tae5431@gmail.com>
2025-06-29 22:10:47 +0900

    adsf

--- Codes_By_Cats/Birman_Python.py
+++ Codes_By_Cats/Birman_Python.py
@@ -1,14 +1,14 @@
 import numpy as np
 # from sympy.plotting import plot3d
-# from sympy import symbols
+from sympy import integrate,symbols,lambdify
+from math import sin
 
 
 
 # a=[1,2,3,4,5]
 # print(sum(a))
-# x,y=symbols('x y')
+x,y=symbols('x y')
 # plot3d((x-y)**2)
-
-def a(b,v):
-    return b,v
-a((1,2))
+print(integrate(x**-5,x))
+f = lambdify(x, x**7)
+print(sin(3.14))
