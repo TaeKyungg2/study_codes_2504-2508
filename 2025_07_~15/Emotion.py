eb0717cc99f99576f2f788a2dc80505153062167
TaeryClassCom <tae5431@gmail.com>
2025-07-14 09:18:35 +0900

    Asdf

--- 2025_07_~15/Emotion.py
+++ 2025_07_~15/Emotion.py
@@ -9,7 +9,7 @@ print(result)
 for face in result:
     x, y, w, h = face['region']['x'], face['region']['y'], face['region']['w'], face['region']['h']
     emotion = max(face['emotion'], key=face['emotion'].get)
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 2)
+    cv2.rectangle(img, (xrudxorhk, y), (x+w, y+h), (0,255,0), 2)
     cv2.putText(img, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
 
 cv2.imshow("Emotion Detection", img)
