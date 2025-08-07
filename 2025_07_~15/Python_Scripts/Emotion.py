3adf6cb0ae2b27b4591bfc10a1f79f664f9f9434
Taery <tae5431@gmail.com>
2025-08-08 02:29:59 +0900

    asdf

--- 2025_07_~15/Python_Scripts/Emotion.py
+++ 2025_07_~15/Python_Scripts/Emotion.py
@@ -1,7 +1,7 @@
 from deepface import DeepFace
 import cv2
 
-img = cv2.imread(r"C:\Users\user\Desktop\asdf.png")  # ← 사진 파일명으로 바꿔줘!
+img = cv2.imread(r"'/Users/Taery/Study_Codes/2025_07_~15/Images/화면 캡처 2025-07-11 135600.png'")  # ← 사진 파일명으로 바꿔줘!
 
 result = DeepFace.analyze(img, actions=['emotion'], enforce_detection=False)
 print(result)
@@ -9,7 +9,6 @@ print(result)
 for face in result:
     x, y, w, h = face['region']['x'], face['region']['y'], face['region']['w'], face['region']['h']
     emotion = max(face['emotion'], key=face['emotion'].get)
-    cv2.rectangle(img, (xrudxorhk, y), (x+w, y+h), (0,255,0), 2)
     cv2.putText(img, emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
 
 cv2.imshow("Emotion Detection", img)
