95d451aca17d1cbc77b26adbbc44f36c56319fe3
TaeryClassCom <tae5431@gmail.com>
2025-08-07 18:25:55 +0900

    asdf

--- Codes_By_Cats/Birman_Python.py
+++ Codes_By_Cats/Birman_Python.py
@@ -1,8 +1,24 @@
-from math import exp
+from math import exp,sin
 from sympy import latex,symbols
 from random import uniform
 import pyautogui
 import time
-time.sleep(2)
-print(pyautogui.position())
+import jax.numpy as jnp
+from jax import jit,grad
+from time import time
 
+def warpper(func,*arg,**kwg):
+    def inn(*arg,**kwg):
+        first=time()
+        func()
+        second=time()
+        return second-first
+@warpper
+@jit
+def time10000(n):
+    sum=0
+    for i in range(n):
+        sum+=sin(n)
+    return sum
+
+print(time10000(10000))
