0b1b85de4e131f1d6f642216580f23cfd286aead
Taery <tae5431@gmail.com>
2025-08-31 16:13:35 +0900

    asdf

--- Codes_By_Cats/Persian.dart
+++ Codes_By_Cats/Persian.dart
@@ -1,11 +1,8 @@
-Widget build(BuildContext context) {
-  return LayoutBuilder(
-    builder: (BuildContext context, BoxConstraints constraints) {
-      if (constraints.maxWidth <= 600) {
-        return _MobileLayout();
-      } else {
-        return _DesktopLayout();
-      }
-    },
-  );
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final res = await http.get(Uri.parse("https://gist.githubusercontent.com/TaeKyungg2/dd77b00e3929e3feb64be5bd411096cf/raw/6bac064316eca99fec93d05d71f9edccfc04e96c/saying.json"));
+  final data = jsonDecode(res.body);
+  print(data[1]);
 }
